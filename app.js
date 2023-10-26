1dad0dcb367fd2e2ff10bc81c711f8595c697cb8
Mohammad Hammad <hammadsmitstudent126507@gmail.com>
2023-10-26 18:47:29 +0500

    Mouse Events

---
+++ app.js
@@ -0,0 +1,17 @@
+let bulb = document.getElementById("img")
+let GetDiv = document.getElementById("getDiv")
+
+function fun(){
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3yq72tBWgZxE8alHb0n9Aue6N3Pm0mccagVJJRvLxhB3nglYxm4AFkjmqAKhWiwi_wNg&usqp=CAU"
+    GetDiv.appendChild("bulb") 
+    
+
+}
+
+
+function out(){
+    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStU9V1YlAHtKP7okuTJiI0z94md5b32bk3vQ&usqp=CAU"
+    GetDiv.appendChild("bulb") 
+}
+
+
